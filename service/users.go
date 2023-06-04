2f7b64bb144837fb788ab83625b6bd6a5b413203
blackestwhite <pesaregoal@gmail.com>
2023-06-04 23:07:24 +0330

    add 'Delete' and 'List' methods for user service

--- service/users.go
+++ service/users.go
@@ -5,9 +5,11 @@ import (
 	"app/entity"
 	"app/utils"
 	"context"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type UserService struct{}
@@ -57,5 +59,40 @@ func (u *UserService) Update(user entity.User, update interface{}) (err error) {
 	}, bson.M{
 		"$set": update,
 	})
+	return
+}
+
+func (u *UserService) Delete(userID string) (err error) {
+	user, err := u.Get(userID)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Client.Database("hala").Collection("users").DeleteOne(context.TODO(), bson.M{
+		"_id": user.ID,
+	})
+	return
+}
+
+func (u *UserService) List(limit, skip int64) (users []entity.User, err error) {
+	opts := options.Find()
+	opts.SetLimit(limit)
+	opts.SetSkip(skip)
+
+	cur, err := db.Client.Database("hala").Collection("users").Find(context.TODO(), bson.M{}, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	for cur.Next(context.TODO()) {
+		var user entity.User
+		decodeErr := cur.Decode(&user)
+		if decodeErr == nil {
+			users = append(users, user)
+		} else {
+			log.Println("error decoding user", err)
+		}
+	}
+
 	return
 }
